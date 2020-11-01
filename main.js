35583ecf8a2b78c97cb2b0630633f0aa1327aa50
AaravGitHub <72292972+AaravGitHub@users.noreply.github.com>
2020-11-01 13:22:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+noseX=0;
+noseY=0;
+rightWristX=0;
+leftWristX=0;
+difference=0;
+
+function setup()
+{
+canvas=createCanvas(550,500);
+canvas.position(650,100);
+video=createCapture(VIDEO);
+video.size(550,500);
+video.position(100,100);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+console.log("poseNet has started");
+}
+
+function gotPoses(results)
+{
+if(results.length>0)
+{
+console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log("noseX= "+noseX+"noseY= "+noseY);
+leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX-rightWristX);
+console.log("leftWristX= "+leftWristX+"rightWristX= "+rightWristX+"difference "+difference);
+}
+}
+
+
+function draw()
+{
+background('#00FF00');
+square(noseX,noseY,difference);
+stroke("#00FFFF");
+fill("#00FFFF");
+document.getElementById("square_side").innerHTML="Width & height of square is   "+difference+"px";
+}
+
+
+
+
+
+
+
+
+
+
